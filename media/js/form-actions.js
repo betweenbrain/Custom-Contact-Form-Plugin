58c7e36cad8bc82aba4fa563b7d9009b86a229be
Matt Thomas <matt@betweenbrain.com>
2014-08-07 10:43:35 -0400

    Target IDs set in XML

--- media/js/form-actions.js
+++ media/js/form-actions.js
@@ -9,14 +9,14 @@
 
 (function ($) {
 	$(document).ready(function () {
-		$('#jform_recipients').change(function () {
-			var recipient = $('#jform_recipients option:selected').val();
+		$('#recipients').change(function () {
+			var recipient = $('#recipients option:selected').val();
 			$('input[name=id]').val(recipient);
 		});
 
-		$('#jform_country').change(function () {
+		$('#country').change(function () {
 			var newOptions = {'State': 'State', 'Other': 'Other'};
-			var selected = $('#jform_country option:selected').val();
+			var selected = $('#country option:selected').val();
 
 			if (selected === 'United States') {
 				newOptions = {'State': 'State', 'Alabama': 'Alabama', 'Alaska': 'Alaska', 'Arizona': 'Arizona', 'Arkansas': 'Arkansas', 'California': 'California', 'Colorado': 'Colorado', 'Connecticut': 'Connecticut', 'Delaware': 'Delaware', 'District Of Columbia': 'District Of Columbia', 'Florida': 'Florida', 'Georgia': 'Georgia', 'Hawaii': 'Hawaii', 'Idaho': 'Idaho', 'Illinois': 'Illinois', 'Indiana': 'Indiana', 'Iowa': 'Iowa', 'Kansas': 'Kansas', 'Kentucky': 'Kentucky', 'Louisiana': 'Louisiana', 'Maine': 'Maine', 'Maryland': 'Maryland', 'Massachusetts': 'Massachusetts', 'Michigan': 'Michigan', 'Minnesota': 'Minnesota', 'Mississippi': 'Mississippi', 'Missouri': 'Missouri', 'Montana': 'Montana', 'Nebraska': 'Nebraska', 'Nevada': 'Nevada', 'New Hampshire': 'New Hampshire', 'New Jersey': 'New Jersey', 'New Mexico': 'New Mexico', 'New York': 'New York', 'North Carolina': 'North Carolina', 'North Dakota': 'North Dakota', 'Ohio': 'Ohio', 'Oklahoma': 'Oklahoma', 'Oregon': 'Oregon', 'Pennsylvania': 'Pennsylvania', 'Rhode Island': 'Rhode Island', 'South Carolina': 'South Carolina', 'South Dakota': 'South Dakota', 'Tennessee': 'Tennessee', 'Texas': 'Texas', 'Utah': 'Utah', 'Vermont': 'Vermont', 'Virginia': 'Virginia', 'Washington': 'Washington', 'West Virginia': 'West Virginia', 'Wisconsin': 'Wisconsin', 'Wyoming': 'Wyoming'};
@@ -26,7 +26,7 @@
 				newOptions = {'State': 'State', 'Alberta': 'Alberta', 'British Columbia': 'British Columbia', 'Manitoba': 'Manitoba', 'New Brunswick': 'New Brunswick', 'Newfoundland and Labrador': 'Newfoundland and Labrador', 'Northwest Territories': 'Northwest Territories', 'Nova Scotia': 'Nova Scotia', 'Nunavut': 'Nunavut', 'Ontario': 'Ontario', 'Prince Edward Island': 'Prince Edward Island', 'Quebec': 'Quebec', 'Saskatchewan': 'Saskatchewan', 'Yukon Territory': 'Yukon Territory'};
 			}
 
-			var select = $('#jform_state');
+			var select = $('#state');
 			select.empty();
 			$.each(newOptions, function (key, value) {
 				select.append($('<option></option>')
